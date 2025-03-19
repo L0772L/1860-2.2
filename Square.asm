d1b54e97bbed598bec1ea0678ba370b14216f58d
L0772L <nsdb0772@leeds.ac.uk>
2025-03-19 20:59:00 +0000

    added work

--- Square.asm
+++ Square.asm
@@ -3,52 +3,25 @@
 D=M      // D = x
 @R1
 M=0      // R1 = 0 (初始化 y=0)
+@R2
+M=D      // R2 = x (循环计数器)
 
-@R0
-D=M
-@NEGATIVE
-D;JLT    // 如果 x < 0，跳转到 NEGATIVE 处理负数
-
-@I
-M=D      // I = x (循环变量)
-@LOOP
+(LOOP)
+@R2
 D=M
 @END
-D;JEQ    // 如果 I == 0，结束
+D;JEQ    // 如果 R2 == 0，结束循环
 
 @R0
-D=M
+D=M      // D = x
 @R1
-M=M+D    // R1 += x (重复 x 次累加)
+M=M+D    // y += x
 
-@I
-M=M-1    // I--
+@R2
+M=M-1    // R2 -= 1
 @LOOP
 0;JMP    // 继续循环
 
 (END)
 @END
 0;JMP    // 结束程序
-
-(NEGATIVE)
-@R0
-D=M
-D=-D     // 取 x 的绝对值
-@I
-M=D      // I = abs(x)
-
-@LOOP_NEG
-D=M
-@END
-D;JEQ    // 如果 I == 0，结束
-
-@R0
-D=M
-D=-D
-@R1
-M=M+D    // R1 += (-x)，负数相乘仍为正数
-
-@I
-M=M-1    // I--
-@LOOP_NEG
-0;JMP    // 继续循环
